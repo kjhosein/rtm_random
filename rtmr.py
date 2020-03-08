b5ed40f7b5859016e3f55871a56e9ba41d7b24b4
Khalid J Hosein <kjhosein@gmail.com>
2020-03-08 12:35:58 -0400

    Initial commit - working MVP

---
+++ rtmr.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python2
+
+import os
+import sys
+import random
+import webbrowser
+from rtmapi import Rtm
+from colorama import init
+init()  # allows colorama to work under Windows
+from colorama import Fore, Back, Style
+
+api_key = "98cfa8464ef953428bc3a8cce7f6f155"
+shared_secret = "9da203dc006ffcc3"
+
+# Highlighting Colors (uses Colorama module):
+# Options:
+# Fore: BLACK, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE, RESET.
+# Back: BLACK, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE, RESET.
+# Style: DIM, NORMAL, BRIGHT
+FG = "CYAN"
+BG = "RESET"
+STYLE = "BRIGHT"
+COLORAMA_FG = getattr(Fore, FG)
+COLORAMA_BG = getattr(Back, BG)
+COLORAMA_STYLE = getattr(Style, STYLE)
+
+
+if __name__ == '__main__':
+    # call the program as `listtasks.py api_key shared_secret [optional: token]`
+    # get those parameters from http://www.rememberthemilk.com/services/api/keys.rtm
+    # api_key, shared_secret = sys.argv[1:3]
+    # token = sys.argv[3] if len(sys.argv) >= 4 else None
+    
+    user_home_dir = os.path.expanduser("~")
+    rtm_auth_file = os.path.join(user_home_dir, '.rtm_auth_token')
+    if os.path.exists(rtm_auth_file):
+        with open(rtm_auth_file, "r") as f:
+            token = f.readline()
+    else:
+        token = None
+
+    api = Rtm(api_key, shared_secret, "read", token)
+
+    # authenication block, see http://www.rememberthemilk.com/services/api/authentication.rtm
+    # check for valid token
+    if not api.token_valid():
+        # use desktop-type authentication
+        url, frob = api.authenticate_desktop()
+        # open webbrowser, wait until user authorized application
+        webbrowser.open(url)
+        raw_input("Continue?")
+        # get the token for the frob
+        api.retrieve_token(frob)
+        # print out new token, should be used to initialize the Rtm object next time
+        # (a real application should store the token somewhere)
+        print "New token: %s" % api.token
+
+        # user_home_dir = expanduser("~")
+        # rtm_auth_file = os.path.join(user_home_dir, '.rtm_auth_token')
+        f = open(rtm_auth_file, "w")
+        f.write(api.token)
+        f.close()
+
+
+# get all open tasks, see http://www.rememberthemilk.com/services/api/methods/rtm.tasks.getList.rtm
+    result = api.rtm.tasks.getList(filter="status:incomplete list:Current priority:1 ")
+    # print "result is a var of type: ", result
+    # print "result is a var of type: ", type(result)
+    # print "result.tasks is a var of type: ", result.tasks
+    # print "\n"
+    # list_of_tasks = list()
+    list_of_tasks = []
+
+    for tasklist in result.tasks:
+        for taskseries in tasklist:
+    #         print taskseries.task.due, taskseries.name
+            # list_of_tasks.append([taskseries.name, taskseries.priority, taskseries.id])
+            list_of_tasks.append(taskseries.name)
+    random_task_name = random.choice(list_of_tasks)
+    
+    # print Style.BRIGHT, Fore.MAGENTA
+    # print random.choice(list_of_tasks)
+    # print Style.RESET_ALL
+    print "DEBUG: random task name is: ", random_task_name  # DEBUG
+    print 
+
+    # Get only the specific task by task_id
+    result = ""
+    result = api.rtm.tasks.getList(filter="name:%s" % random_task_name)
+    for tasklist in result.tasks:
+        for taskseries in tasklist:
+            # print taskseries.task.due, taskseries.name, taskseries.priority, tasklist
+            print 'Task Name: \t', COLORAMA_STYLE, COLORAMA_BG, COLORAMA_FG, taskseries.name, Style.RESET_ALL
+            print 'Priority: \t', COLORAMA_STYLE, COLORAMA_BG, COLORAMA_FG, taskseries.priority, Style.RESET_ALL
+            print 'Due: \t\t', COLORAMA_STYLE, COLORAMA_BG, COLORAMA_FG, taskseries.due, Style.RESET_ALL
+    print
