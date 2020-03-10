5b06db99fe85a8e813b24ee34f1a922af7a492c5
Khalid J Hosein <kjhosein@gmail.com>
2020-03-10 15:09:22 -0400

    Strip the filter down to only 'incomplete' tasks. General clean up & add comments.

--- rtmr.py
+++ rtmr.py
@@ -10,7 +10,9 @@ import webbrowser
 from rtmapi import Rtm
 from colorama import init
 from colorama import Fore, Back, Style
+
 init()  # allows colorama to work under Windows
+
 # The next 2 lines are workarounds for a bug in the datetime module.
 # reference: https://stackoverflow.com/a/43043036/389946
 strptime = datetime.strptime
@@ -34,14 +36,13 @@ COLORAMA_STYLE = getattr(Style, STYLE)
 
 def main(raw_args=None):
     # Check the arguments passed to this script
-    parser = argparse.ArgumentParser(description='Get a random TO DO from RTM!', prefix_chars='-/')
+    parser = argparse.ArgumentParser(description='Get a random TO DO from Remember the Milk!', prefix_chars='-/')
     parser.add_argument('--loglevel', dest='loglevel', metavar='',
                         choices=['debug'], type=str.lower,
                         help="[optional] Set the log level (e.g. debug, etc.)")
     args = parser.parse_args(raw_args)
 
-    # Set loglevel if --loglevel argument is used, otherwise set to INFO:
-    # if args.loglevel is not None:
+    # Set loglevel if --loglevel argument is used, otherwise set to INFO
     if args.loglevel is None:
         logging_num_level = 20
     else:
@@ -52,6 +53,7 @@ def main(raw_args=None):
 
     logging.debug("Args passed in are: " + str(args))
 
+    # Look for a token in ~/.rtm_auth_token first
     user_home_dir = os.path.expanduser("~")
     rtm_auth_file = os.path.join(user_home_dir, '.rtm_auth_token')
     if os.path.exists(rtm_auth_file):
@@ -60,10 +62,11 @@ def main(raw_args=None):
     else:
         token = None
 
-    api = Rtm(api_key, shared_secret, "read", token)
+    # Create a class instance using the RtmAPI module
+    api = Rtm(api_key, shared_secret, "read", token, api_version=2)
 
-    # authenication block, see http://www.rememberthemilk.com/services/api/authentication.rtm
-    # check for valid token
+    # Authenication block, see http://www.rememberthemilk.com/services/api/authentication.rtm
+    # Check for valid token. If none, open a browser so the user can authenticate.
     if not api.token_valid():
         # use desktop-type authentication
         url, frob = api.authenticate_desktop()
@@ -76,38 +79,47 @@ def main(raw_args=None):
         # (a real application should store the token somewhere)
         logging.debug("New token: %s" % api.token)
 
-        # user_home_dir = expanduser("~")
-        # rtm_auth_file = os.path.join(user_home_dir, '.rtm_auth_token')
+        # Write out the token to the user's home directory
         f = open(rtm_auth_file, "w")
         f.write(api.token)
         f.close()
 
-    # get all open tasks, see http://www.rememberthemilk.com/services/api/methods/rtm.tasks.getList.rtm
-    result = api.rtm.tasks.getList(filter="status:incomplete list:Current priority:1 ")
+    # Get all incomplete tasks based on the constructed filter.
+    # RTM filters: https://www.rememberthemilk.com/help/?ctx=basics.search.advanced
+    filter = "status:incomplete"
+    result = api.rtm.tasks.getList(filter="%s" % filter)
     list_of_tasks = []
 
+    # Use the RtmAPI tasks iter to put the filtered set of tasks into a list
     for tasklist in result.tasks:
         for taskseries in tasklist:
             list_of_tasks.append(taskseries.name)
-    random_task_name = random.choice(list_of_tasks)
-    print '# of tasks is: ', len(list_of_tasks)
 
+    # Pick out a random task name
+    random_task_name = random.choice(list_of_tasks)
     logging.debug("Random task name is: " + random_task_name)
 
-    # Get only the specific task by task_id
+    # Get only the specific task by task_id along with its Priority & Due Date and print them out.
     result = ""
     result = api.rtm.tasks.getList(filter="name:%s" % random_task_name)
     for tasklist in result.tasks:
         for taskseries in tasklist:
-            print 'Task Name: \t', COLORAMA_STYLE, COLORAMA_BG, COLORAMA_FG, taskseries.name, Style.RESET_ALL
-            print 'Priority: \t', COLORAMA_STYLE, COLORAMA_BG, COLORAMA_FG, taskseries.task.priority, Style.RESET_ALL
-            logging.debug('The type of taskseries.task.due is ...' + str(type(taskseries.task.due)))
+            print "\nTask Name: \t", COLORAMA_STYLE, COLORAMA_BG, COLORAMA_FG, taskseries.name, Style.RESET_ALL
+
+            if "N" in taskseries.task.priority:
+                print 'Priority: \t', COLORAMA_STYLE, COLORAMA_BG, COLORAMA_FG, 'None', Style.RESET_ALL
+            else:
+                print 'Priority: \t', COLORAMA_STYLE, COLORAMA_BG, COLORAMA_FG, taskseries.task.priority, Style.RESET_ALL
+
             if taskseries.task.due == '':
-                print 'Due: \t\t-'
+                print 'Due: \t\t -'
             else:
                 formatted_date = strftime(strptime(taskseries.task.due, "%Y-%m-%dT%H:%M:%SZ"), "%d %b %Y")
                 print 'Due: \t\t', COLORAMA_STYLE, COLORAMA_BG, COLORAMA_FG, formatted_date, Style.RESET_ALL
-    print
+
+    # As a bonus, print the # of tasks in the user's search filter
+    print "\n The total # of tasks with your filter of '%s': " % filter, COLORAMA_STYLE, COLORAMA_BG, COLORAMA_FG, \
+        len(list_of_tasks), Style.RESET_ALL, "\n"
 
 
 if __name__ == '__main__':
